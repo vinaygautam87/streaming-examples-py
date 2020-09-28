4d91f1e8e57ce2f6df70d6f247efd1929483c49c
SidharthaRay <sidhartha.ray533@gmail.com>
2020-09-28 18:43:10 +0530

    initial commit

--- com/dsm/kafka/append_mode_demo.py
+++ com/dsm/kafka/append_mode_demo.py
@@ -44,8 +44,7 @@ if __name__ == '__main__':
         .writeStream\
         .outputMode("append")\
         .format("console") \
-        .option("checkpointLocation",
-                "s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/" + app_conf["kafka"]["checkpoint_dir"]) \
+        .option("checkpointLocation", "s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/" + app_conf["kafka"]["checkpoint_dir"]) \
         .start()\
         .awaitTermination()
 
