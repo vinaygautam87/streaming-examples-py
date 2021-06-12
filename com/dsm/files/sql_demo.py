0612972a9225060f2b312ee31034bacf034c2149
transform <manivinay@gmail.com>
2021-06-13 01:20:08 +0530

    master change

--- com/dsm/files/sql_demo.py
+++ com/dsm/files/sql_demo.py
@@ -11,7 +11,6 @@ if __name__ == '__main__':
     spark = SparkSession \
         .builder \
         .appName("Read from enterprise applications") \
-        .master('local[*]') \
         .getOrCreate()
     spark.sparkContext.setLogLevel('ERROR')
 
